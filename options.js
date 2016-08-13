c22cd2cdbe5d6cf99f45bf3ed957ef9ecf814d15
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2016-08-13 17:38:08 -0400

    Options page improvements
    - Add advanced section
    - Add style preview

--- options.js
+++ options.js
@@ -1,31 +1,8 @@
+// Standard Options
 const fixLinks   = document.getElementById("fixLinks");
 const inlineVids = document.getElementById("inlineVids");
 const fixVideos  = document.getElementById("fixVideos");
 const useStyle   = document.getElementById("useStyle");
-const modStyle   = document.getElementById("modStyle");
-
-const defaultOptions = {
-    "fixLinks":   true,
-    "inlineVids": false,
-    "fixVideos":  true,
-    "useStyle":   true,
-    "modStyle":   "border: 1px dashed green"
-};
-
-const storage = chrome.storage.local;
-function init() {
-    storage.get(defaultOptions, function(opts) {
-        fixLinks.checked   = opts.fixLinks;
-        inlineVids.checked = opts.inlineVids;
-        fixVideos.checked  = opts.fixVideos;
-        useStyle.checked   = opts.useStyle;
-        modStyle.value     = opts.modStyle;
-
-        inlineVids.disabled    = !fixLinks.checked;
-        modStyle.style.display = useStyle.checked ? '' : 'none';
-    });
-}
-init();
 
 fixLinks.addEventListener("change", function(e) {
     storage.set({"fixLinks": this.checked});
@@ -42,14 +19,59 @@ fixVideos.addEventListener("change", function(e) {
 
 useStyle.addEventListener("change", function(e) {
     storage.set({"useStyle": this.checked});
-    modStyle.style.display = this.checked ? '' : 'none';
-});
-
-modStyle.addEventListener("change", function(e) {
-    storage.set({"modStyle": this.value});
+    modStyle.disabled = !this.checked;
 });
 
 document.getElementById("reset").addEventListener("click", function(e) {
     storage.clear();
     init();
 });
+
+// Expert Options
+const modStyle     = document.getElementById("modStyle");
+const preview      = document.getElementById("preview");
+const expertOpts   = document.getElementById("expertOpts");
+const toggleExpert = document.getElementById("toggleExpert");
+
+modStyle.addEventListener("change", function(e) {
+    storage.set({"modStyle": this.value});
+    preview.style.cssText = this.value;
+});
+
+toggleExpert.visible = false;
+toggleExpert.addEventListener("click", function(e) {
+    this.visible = !this.visible;
+    if (this.visible) {
+        expertOpts.style.display = "initial";
+        this.innerText = "Hide Expert Options";
+    } else {
+        expertOpts.style.display = "none";
+        this.innerText = "Show Expert Options";
+    }
+});
+
+
+// Init
+const defaultOptions = {
+    "fixLinks":   true,
+    "inlineVids": false,
+    "fixVideos":  true,
+    "useStyle":   true,
+    "modStyle":   "border: 1px dashed green"
+};
+const storage = chrome.storage.local;
+function init() {
+    storage.get(defaultOptions, function(opts) {
+        fixLinks.checked   = opts.fixLinks;
+        inlineVids.checked = opts.inlineVids;
+        fixVideos.checked  = opts.fixVideos;
+        useStyle.checked   = opts.useStyle;
+        modStyle.value     = opts.modStyle;
+
+        preview.style.cssText = modStyle.value;
+
+        inlineVids.disabled = !fixLinks.checked;
+        modStyle.disabled   = !useStyle.checked;
+    });
+}
+init();
