c04ffee9dee28ca8fa0df1bc7ce21dfe642814b6
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2017-04-25 00:50:35 -0400

    v1.4.0 - Add icon, page action, and sync option

--- init.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    // Only load once at beginning for consistency
-    if (typeof(chrome) !== "undefined" && chrome.storage) {
-        chrome.storage.sync.get(defaultOptions, FBTR.init);
-    } else {
-        FBTR.init(defaultOptions);
-    }
-}());
