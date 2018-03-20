e82744bb0a7dc33ef355ac399c582681a5731383
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2018-03-20 12:08:15 -0700

    Add license

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+/*  This file is part of FacebookTrackingRemoval.
+
+    FacebookTrackingRemoval is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FacebookTrackingRemoval is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FacebookTrackingRemoval.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright (C) 2016-2018 Michael Ziminsky
+*/
+
 'use strict';
 app.init().then(() => {
     if (!app.options.enabled)
