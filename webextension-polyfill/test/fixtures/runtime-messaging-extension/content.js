19b60b8c92bdaef12eca38d055773c0381f73ac2
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2018-04-21 14:29:18 -0700

    v1.5.7.1 - Convert webextension-polyfill to submodule

--- webextension-polyfill/test/fixtures/runtime-messaging-extension/content.js
+++
@@ -1,9 +0,0 @@
-const {name} = browser.runtime.getManifest();
-
-console.log(name, "content script loaded");
-
-browser.runtime.sendMessage("content script message").then(reply => {
-  console.log(name, "content script received reply", {reply});
-});
-
-console.log(name, "content script message sent");
