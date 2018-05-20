f22cde8a0fd70c594d8df0233bb4e8c968d6d832
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2018-05-20 10:01:15 -0700

    v1.5.13 - Bump version

--- package.sh
+++ package.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+set -x
+
 cd $(dirname $0)
-zip -r -ll extension.zip . -i 'src/*' -i browser-polyfill.min.js -i manifest.json -i CHANGELOG -i LICENSE
+
+zip -r -ll Firefox_Extension.zip . -i 'src/*' browser-polyfill.min.js manifest.json CHANGELOG LICENSE
+
+tempDir=`mktemp -d`
+trap "rm -rf $tempDir" EXIT
+
+cp Firefox_Extension.zip Chrome_Extension.zip
+chromePath="$(pwd)/Chrome_Extension.zip"
+
+sed -E '/"name":/s;(Facebook\S*) ([^"]+);\2 for \1;' manifest.json > "$tempDir/manifest.json"
+cd "$tempDir"
+zip -f -ll "$chromePath" manifest.json
