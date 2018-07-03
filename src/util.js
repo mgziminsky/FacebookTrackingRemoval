02a327d1d2e550d23308d8ed3ccc951bb513ecef
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2018-07-03 16:04:48 -0700

    v1.5.15.1 - Fix menu items

--- src/util.js
+++ src/util.js
@@ -21,7 +21,7 @@
 const PROCESSED_CLASS = "FBTR-PROCESSED";
 
 const ALLOWED_CLICK_ELEMENTS = ["INPUT", "SELECT", "BUTTON", "TEXTAREA"];
-const ALLOWED_ROLES = ["BUTTON", "MENUITEM"];
+const ALLOWED_ROLES = ["BUTTON"];
 const ALLOWED_SELECTOR = [
     "a[data-jsid='actionLink']", "a[ajaxify]", "a.see_more_link", "form div.pam", // Desktop
     "span[data-sigil='more']", // Mobile
