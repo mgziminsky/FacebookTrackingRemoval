6366733608a8c657368b584512bf316f730b2882
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2019-10-14 15:20:17 -0700

    New dynamic sponsored selector

--- package.sh
+++ package.sh
@@ -4,4 +4,5 @@ set -x
 cd $(dirname $0)
 
 rm -f Browser_Extension.zip
+cp webextension-polyfill/dist/browser-polyfill.min.js* ./
 zip -r -ll Browser_Extension.zip . -i 'src/*' browser-polyfill.min.js manifest.json CHANGELOG LICENSE
