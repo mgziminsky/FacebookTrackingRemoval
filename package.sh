9de1bb9ce86ea26d2f70330950883961874dc759
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2023-01-18 17:14:30 -0700

    Move rules out of src to top-level data dir

--- package.sh
+++ package.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)
 
 rm -f Browser_Extension.zip
 cp webextension-polyfill/dist/browser-polyfill.min.js* ./
-zip -r -ll Browser_Extension.zip . -i 'src/*' '_locales/*' browser-polyfill.min.js manifest.json CHANGELOG LICENSE
+zip -r -ll Browser_Extension.zip . -i 'src/*' '_locales/*' 'data/*' browser-polyfill.min.js manifest.json CHANGELOG LICENSE
